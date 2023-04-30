ac4573d80d8b2a244b9f41a20924976be8660add
Kenny Zhu Ye <kennyzhu@MacBook-Air-de-Kenny.local>
2023-04-30 18:32:38 -0500

    implementacion por ahora

---
+++ sorting_algorithms.py
@@ -0,0 +1,73 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+def heap_sort(arr):
+    def heapify(arr, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and arr[largest] < arr[left]:
+            largest = left
+
+        if right < n and arr[largest] < arr[right]:
+            largest = right
+
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            heapify(arr, n, largest)
+
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+    return arr
+
+def quick_sort(arr, pivot='left'):
+    if len(arr) <= 1:
+        return arr
+
+    if pivot == 'left':
+        pivot_value = arr[0]
+    elif pivot == 'right':
+        pivot_value = arr[-1]
+    else:
+        raise ValueError("Invalid pivot option")
+
+    less = [x for x in arr if x < pivot_value]
+    equal = [x for x in arr if x == pivot_value]
+    greater = [x for x in arr if x > pivot_value]
+
+    return quick_sort(less, pivot) + equal + quick_sort(greater, pivot)
