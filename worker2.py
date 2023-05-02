df989f8ab2a55cf07ad3ed38ee999d9827e20934
K3nnyZY <kennyzhuye15@gmail.com>
2023-05-02 11:42:01 -0500

    c

--- worker2.py
+++ worker2.py
@@ -32,11 +32,7 @@ def process_request(conn, addr, worker_id, other_worker_addr):
 
             tiempo = round(end_time - start_time, 5)
 
-            response = {
-                "worker_id": worker_id,
-                "arr": vector,
-                "Flag": tiempo
-            }
+            response = {"worker_id": worker_id, "Flag": tiempo, "arr": vector}
             send_data(conn, response)
 
         except (json.decoder.JSONDecodeError, ConnectionResetError):
